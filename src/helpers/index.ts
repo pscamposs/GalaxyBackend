92f72bc4d6db09c537892bfb670eca43bc0e7a2d
pscamposs <pscampos221@gmail.com>
2024-07-12 15:13:19 -0300

    feat: jwt secret env

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,9 +1,11 @@
-import crypto from 'crypto'
-
-const SECRET = 'D-201-2KO';
-export const random = () => crypto.randomBytes(128).toString('base64');
+import crypto from "crypto";
+import dotenv from "dotenv";
+dotenv.config();
+const SECRET = process.env.JWT_SECRET as string;
+export const random = () => crypto.randomBytes(128).toString("base64");
 export const authentication = (salt: string | undefined, password: string) => {
-    return crypto.createHmac('sha256', [salt, password].join('/')).update(SECRET).digest('hex');
-}
-
- 
+  return crypto
+    .createHmac("sha256", [salt, password].join("/"))
+    .update(SECRET)
+    .digest("hex");
+};
